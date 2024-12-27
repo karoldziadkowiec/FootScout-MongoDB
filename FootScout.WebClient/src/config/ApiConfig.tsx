2488dfbc8c8f420e02a971624301c291bd08ec04
karoldziadkowiec <karol.dziadkkowiec@gmail.com>
2024-12-27 14:19:04 +0100

    project finished

---
+++ FootScout.WebClient/src/config/ApiConfig.tsx
@@ -0,0 +1,4 @@
+const ApiPORT: number = 7272;    // MongoDB
+
+const ApiURL: string = `https://localhost:${ApiPORT}/api`;
+export default ApiURL;
---
+++ FootScout.WebClient/src/config/ChatHubConfig.tsx
@@ -0,0 +1,6 @@
+const ApiPORT: number = 7272;    // MongoDB
+
+const HubName: string = 'chathub';
+
+const ChatHubURL: string = `https://localhost:${ApiPORT}/${HubName}`;
+export default ChatHubURL;
